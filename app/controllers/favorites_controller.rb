92f1f20b290e85f94d11f897b78a93655f154808
yuna1001 <hegapoo1001@gmail.com>
2020-03-04 04:16:27 +0000

    [Add] booksのindexページにいいね機能を追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,14 +3,16 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
+    @url = request.referer
+    redirect_to @url
   end
 
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to book_path(book)
+    @url = request.referer
+    redirect_to @url
   end
 
 
